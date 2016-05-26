8d3e4db7dd67e2d6feccf44f53b286197c0c6a09
Nguyen <knguy13@urmc-sh.rochester.edu>
2016-05-26 18:00:28 -0400

    Started work on angular stuff

--- admin.php
+++ admin.php
@@ -16,15 +16,41 @@
 	<title>Admin Panel - Thorium VM Management</title>
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<link rel="stylesheet" href="./styles/admin.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js"></script>
-	<script src="js/main.js"></script>
+	<script src="js/app.js"></script>
 </head>
 <body>
 
+	<div class="container-fluid">
+		<div class="row header">
+			<p>Thorium VM Management</p>
+		</div>
+		<div class="row subheader">
+			<p>Welcome, <?php echo $_SESSION['admin'] ?>! <a href="logout.php">(Logout)</a></p>
+		</div>
+		<!--angular application-->
+		<div ng-app="adminpanel" class="row application">
+				<!--VM List-->
 
-	<h1>Welcome, <?php echo $_SESSION['admin'] ?>! <a href="logout.php">Logout</a></h1>
-	<div ng-app class="container-fluid">
-		<p>1 + 2 = {{1+2}}</p>
+				<div ng-controller="listController" class="vmlist col-md-3">
+					<div class="row vmsearch">
+						<input type="text" ng-model="$ctrl.query" class="form-control" placeholder="Search">
+					</div>
+					<div class="row vmlisting">
+						<p> + Add a VM</p>
+					</div>
+					<div ng-repeat="vm in vmlist | filter: $ctrl.query" class="row vmlisting {{selected}}" ng-click="setSelected(this);setVM(this)">
+							{{vm}}
+					</div>
+
+				</div>
+
+				<!--VM Detail window-->
+				<div ng-controller="vmDetailController" class="details col-md-9">
+				{{vmname}}
+				<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
+		</div>
 	</div>
 	
 
--- js/app.js
+++ js/app.js
@@ -1,1 +1,42 @@
-<script></script>
+var myapp = angular.module('adminpanel', []);
+
+// Watch for changes in ListController and refresh VM Controller
+myapp.factory("Service", function () {
+    var vmname = "none";
+     function getVM() {
+        return vmname;
+    }
+    function setVM(name) {
+        vmname = name;
+    }
+    return {
+        getVM: getVM,
+        setVM: setVM,
+    }
+});
+
+myapp.controller("listController", function listController($scope, $http, Service){
+	$scope.setVM = Service.setVM;
+
+	$http({method:'GET', url:"./svc/svc.php?svc=getAllVMs"}).then(function successCallback(n){
+		$scope.vmlist = n.data;
+		// console.log(n);
+	});
+
+	$scope.setSelected = function() {
+	   // console.log("show", arguments, this);
+	   if ($scope.lastSelected) {
+	     $scope.lastSelected.selected = '';
+	   }
+	   this.selected = 'selected';
+	   $scope.lastSelected = this;
+	}
+});
+
+myapp.controller("vmDetailController", function vmDetailController($scope, $http, Service){
+	 $scope.$watch(function () { return Service.getVM();}, function (value) {
+	 		//console.log(value);
+	        $scope.vmname = value.vm;
+	    });
+});
+
